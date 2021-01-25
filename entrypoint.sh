3f20d1d713fa7ea42236365c24852b6d69770356
Roberto Santalla <rsantalla@newrelic.com>
2021-01-25 14:11:41 +0100

    quote messages, subshell

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ errors=0
 if [[ -z $POST_INSTALL ]]; then
     POST_INSTALL="
     test -e /var/db/newrelic-infra/newrelic-integrations/bin/${INTEGRATION}
-    /var/db/newrelic-infra/newrelic-integrations/bin/${INTEGRATION} -show_version | grep -e "$TAG"
+    /var/db/newrelic-infra/newrelic-integrations/bin/${INTEGRATION} -show_version 2>&1 | grep -e $TAG
     "
 fi
 
@@ -19,9 +19,9 @@ function build_and_test() {
 
     echo "Running post-installation checks"
     echo "$POST_INSTALL" | while read check; do
-      if ! echo "$check" | docker run --rm -i $INTEGRATION:$distro-$TAG; then
+      if ! ( echo "$check" | docker run --rm -i "$INTEGRATION:$distro-$TAG" ); then
         echo "$check"
-        echo ❌ Failed for $INTEGRATION:$distro-$TAG
+        echo "❌ Failed for $INTEGRATION:$distro-$TAG"
         return 1
       fi
     done
