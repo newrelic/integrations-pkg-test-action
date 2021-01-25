a38961b9f647e72891c97fbf55a4ced056859cd1
Roberto Santalla <rsantalla@newrelic.com>
2021-01-25 14:07:25 +0100

    add some emoji

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,35 +12,36 @@ fi
 
 function build_and_test() {
     if ! docker build -t "$INTEGRATION:$distro-$TAG" -f $GITHUB_ACTION_PATH/dockerfiles-test/Dockerfile-$distro --build-arg TAG="${TAG}" --build-arg INTEGRATION="${INTEGRATION}" --build-arg UPGRADE=$1 .; then
-        echo "Clean install failed on $distro" 1>&2
+        echo "❌ Clean install failed on $distro" 1>&2
         return 1
     fi
-    echo "Installation succeeded install done"
+    echo "✅ Installation for $INTEGRATION:$distro-$TAG succeeded"
 
     echo "Running post-installation checks"
     echo "$POST_INSTALL" | while read check; do
       if ! echo "$check" | docker run --rm -i $INTEGRATION:$distro-$TAG; then
         echo "$check"
-        echo Failed for $INTEGRATION:$distro-$TAG
+        echo ❌ Failed for $INTEGRATION:$distro-$TAG
         return 1
       fi
     done
+    echo "✅ Post-installation checks for $INTEGRATION:$distro-$TAG succeeded"
 }
 
 for distro in centos debian suse; do
-    echo "Building base image for $distro..."
+    echo "ℹ️ Building base image for $distro..."
     docker build -t "$distro-test" -f $GITHUB_ACTION_PATH/dockerfiles-base/Dockerfile-base-$distro .
 
-    echo "Testing clean install"
+    echo "ℹ️ Testing clean install"
     build_and_test false
     ((errors += $? ))
 
     if [[ $UPGRADE == "true" ]]; then
-        echo "Testing upgrade path"
+        echo "ℹ️ Testing upgrade path"
         build_and_test true
         ((errors += $? ))
     else
-        echo "Skipping upgrade path"
+        echo "ℹ️ Skipping upgrade path"
     fi
 done
 
