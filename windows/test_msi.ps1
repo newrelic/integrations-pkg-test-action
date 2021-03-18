9a6c04476d9cce3a352482b811d666ab9e4701ab
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2021-03-18 12:32:19 +0100

    feat: add exe package test instalation

--- windows/test_msi.ps1
+++ windows/test_msi.ps1
@@ -4,30 +4,45 @@ param (
     [string]$ARCH="amd64",
     [string]$TAG="v0.0.0",
     [string]$UPGRADE="false", # upgrade: upgrade msi from last released version.
-    [string]$MSI_PATH=""
+    [string]$MSI_PATH="",
+    [string]$MSI_FILE_NAME="",
+    [string]$LATEST_MSI_NAME=""
+    [string]$LATEST_MSI_URL=""
 )
+
+if($LATEST_MSI_NAME -eq "")
+{
+    $LATEST_MSI_NAME="${INTEGRATION}-${ARCH}.msi"   
+}
+if($LATEST_MSI_URL -eq "")
+{
+    $LATEST_MSI_URL="https://download.newrelic.com/infrastructure_agent/windows/integrations/${INTEGRATION}/${LATEST_MSI_NAME}"
+}
 
 if($UPGRADE -eq "true")
 {
-    $latest_msi_name = "${INTEGRATION}-${ARCH}.msi"
-    $latest_msi_url = "https://download.newrelic.com/infrastructure_agent/windows/integrations/${INTEGRATION}/${latest_msi_name}"
-    write-host "===> Downloading latest released version of msi from ${latest_msi_url}"
+    write-host "===> Downloading latest released version of msi from ${LATEST_MSI_URL}"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     try {
-        Invoke-WebRequest "${latest_msi_url}" -OutFile "${latest_msi_name}"
+        Invoke-WebRequest "${LATEST_MSI_URL}" -OutFile "${LATEST_MSI_NAME}"
     } catch {
-        write-host "⚠️ Couldn't fetch latest version from ${latest_msi_url}, skipping test"
+        write-host "⚠️ Couldn't fetch latest version from ${LATEST_MSI_URL}, skipping test"
         exit 0
     }
 
-    write-host "===> Installing latest released version of msi from ${latest_msi_url}"
-    $p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v msi_log /i ${latest_msi_name}"
+    write-host "===> Installing latest released version of msi from ${LATEST_MSI_URL}"
+    if( $out -notlike "*.msi"){
+        $p = Start-Process ${LATEST_MSI_NAME} -Wait -PassThru -ArgumentList "/s /l installer_log"
+    } else {
+        $p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v installer_log /i ${LATEST_MSI_NAME}"
+    }
     if($p.ExitCode -ne 0)
     {
-        Get-Content -Path .\msi_log
+        Get-Content -Path .\installer_log
         echo "Failed installing latest version of the msi"
         exit 1
     }
+
 }
 
 $version = $TAG -replace "v", ""
@@ -35,12 +50,20 @@ if($MSI_PATH -eq "")
 {
     $MSI_PATH="src\github.com\newrelic\${INTEGRATION}\build\package\windows\nri-${ARCH}-installer\bin\Release"    
 }
-$msi_name = "$MSI_PATH\${INTEGRATION}-${ARCH}.${version}.msi"
+if($MSI_FILE_NAME -eq "")
+{
+    $MSI_FILE_NAME="${INTEGRATION}-${ARCH}.${version}.msi"   
+}
+$msi_name = "${MSI_PATH}\${MSI_FILE_NAME}"
 write-host "===> Installing generated msi: ${msi_name}"
-$p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v msi_log /i ${msi_name}"
+if( $out -notlike "*.msi"){
+    $p = Start-Process ${msi_name} -Wait -PassThru -ArgumentList "/s /l installer_log"
+} else {
+    $p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v installer_log /i ${msi_name}"
+}
 if($p.ExitCode -ne 0)
 {
-    Get-Content -Path .\msi_log
+    Get-Content -Path .\installer_log
     echo "Failed installing the msi"
     exit 1
 }
