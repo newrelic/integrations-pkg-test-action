6cafa5503c36d12847374d1dba9e12d44229aa1e
Roberto Santalla <roobre@users.noreply.github.com>
2021-03-19 13:57:41 +0100

    windows: allow to specify pkgType without overriding the name (#13)
    * windows: allow to specify pkgType without overriding the name
    
    * typo
    
    * minor escaping
    
    * better input validation, safer path joining

--- windows/test_msi.ps1
+++ windows/test_msi.ps1
@@ -3,43 +3,55 @@ param (
     [string]$ARCH = "amd64",
     [string]$TAG = "v0.0.0",
     [string]$UPGRADE = "false", # upgrade: upgrade msi from last released version.
+    [ValidateSet("msi", "exe")]
+    [string]$PKG_TYPE = "msi",
     [string]$PKG_DIR = "",
     [string]$PKG_NAME = "",
-    [string]$PKG_LATEST_NAME = "",
-    [string]$PKG_LATEST_URL = ""
+    [string]$PKG_UPSTREAM_URL_BASE = "",
+    [string]$PKG_UPSTREAM_NAME = ""
 )
 
-if ($PKG_LATEST_NAME -eq "")
+if ($PKG_UPSTREAM_NAME -eq "")
 {
-    $PKG_LATEST_NAME = "${INTEGRATION}-${ARCH}.msi"
+    if ($PKG_TYPE -eq "msi")
+    {
+        $PKG_UPSTREAM_NAME = "${INTEGRATION}-${ARCH}.msi"
+    }
+    elseif ($PKG_TYPE -eq "exe")
+    {
+        $PKG_UPSTREAM_NAME = "${INTEGRATION}-${ARCH}-installer.exe"
+    }
 }
-if ($PKG_LATEST_URL -eq "")
+
+if ($PKG_UPSTREAM_URL_BASE -eq "")
 {
-    $PKG_LATEST_URL = "https://download.newrelic.com/infrastructure_agent/windows/integrations/${INTEGRATION}/${PKG_LATEST_NAME}"
+    $PKG_UPSTREAM_URL_BASE = "https://download.newrelic.com/infrastructure_agent/windows/integrations/${INTEGRATION}/"
 }
+
+$PKG_UPSTREAM_URL = "${PKG_UPSTREAM_URL_BASE}${PKG_UPSTREAM_NAME}"
 
 if ($UPGRADE -eq "true")
 {
-    write-host "ℹ️ Downloading latest released version of msi from ${PKG_LATEST_URL}"
+    write-host "ℹ️ Downloading latest released version of msi from ${PKG_UPSTREAM_URL}"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     try
     {
-        Invoke-WebRequest "${PKG_LATEST_URL}" -OutFile "${PKG_LATEST_NAME}"
+        Invoke-WebRequest "${PKG_UPSTREAM_URL}" -OutFile "${PKG_UPSTREAM_NAME}"
     }
     catch
     {
-        write-host "⚠️ Couldn't fetch latest version from ${PKG_LATEST_URL}, skipping test"
+        write-host "⚠️ Couldn't fetch latest version from ${PKG_UPSTREAM_URL}, skipping test"
         exit 0
     }
 
-    write-host "::group::ℹ️ Installing latest released version of msi from ${PKG_LATEST_URL}"
-    if ($PKG_LATEST_NAME -notlike "*.msi")
+    write-host "::group::ℹ️ Installing latest released version of msi from ${PKG_UPSTREAM_URL}"
+    if ($PKG_TYPE -eq "exe")
     {
-        $p = Start-Process "$PKG_LATEST_NAME" -Wait -PassThru -ArgumentList "/s /l installer_log"
+        $p = Start-Process "$PKG_UPSTREAM_NAME" -Wait -PassThru -ArgumentList "/s /l installer_log"
     }
-    else
+    elseif ($PKG_TYPE -eq "msi")
     {
-        $p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v installer_log /i $PKG_LATEST_NAME"
+        $p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v installer_log /i $PKG_UPSTREAM_NAME"
     }
     Get-Content -Path .\installer_log
     write-host "::endgroup::"
@@ -48,27 +60,35 @@ if ($UPGRADE -eq "true")
         echo "❌ Failed installing latest version of the msi"
         exit 1
     }
-    echo "✅ Installation for ${PKG_LATEST_NAME} succeeded"
+    echo "✅ Installation for ${PKG_UPSTREAM_NAME} succeeded"
 }
 
-$version = $TAG -replace "v", ""
+$version = "$TAG" -replace "v", ""
 if ($PKG_DIR -eq "")
 {
     $PKG_DIR = "src\github.com\newrelic\${INTEGRATION}\build\package\windows\nri-${ARCH}-installer\bin\Release"
 }
 if ($PKG_NAME -eq "")
 {
-    $PKG_NAME = "${INTEGRATION}-${ARCH}.${version}.msi"
+    if ($PKG_TYPE -eq "msi")
+    {
+        $PKG_NAME = "${INTEGRATION}-${ARCH}.${version}.msi"
+    }
+    elseif ($PKG_TYPE -eq "exe")
+    {
+        $PKG_NAME = "${INTEGRATION}-${ARCH}-installer.${version}.exe"
+    }
 }
-$PKG_LATEST_NAME = "${PKG_DIR}\${PKG_NAME}"
-write-host "::group::ℹ️ Installing generated msi: ${PKG_LATEST_NAME}"
-if ($PKG_LATEST_NAME -notlike "*.msi")
+$PKG_PATH = Join-Path -Path "$PKG_DIR" -ChildPath "$PKG_NAME"
+
+write-host "::group::ℹ️ Installing generated msi: ${PKG_PATH}"
+if ($PKG_TYPE -eq "exe")
 {
-    $p = Start-Process "$PKG_LATEST_NAME" -Wait -PassThru -ArgumentList "/s /l installer_log"
+    $p = Start-Process "$PKG_PATH" -Wait -PassThru -ArgumentList "/s /l installer_log"
 }
-else
+elseif ($PKG_TYPE -eq "msi")
 {
-    $p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v installer_log /i ${PKG_LATEST_NAME}"
+    $p = Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/qn /L*v installer_log /i ${PKG_PATH}"
 }
 Get-Content -Path .\installer_log
 write-host "::endgroup::"
@@ -78,7 +98,7 @@ if ($p.ExitCode -ne 0)
     echo "❌ Failed installing the msi"
     exit 1
 }
-echo "✅ Installation for ${PKG_LATEST_NAME} succeeded"
+echo "✅ Installation for ${PKG_PATH} succeeded"
 
 $nr_base_dir = "${env:ProgramFiles}\New Relic\newrelic-infra"
 if ($ARCH -eq "386")
