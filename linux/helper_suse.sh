beb8039f59fc825750d6af9cb75189a05f69b625
Roberto Santalla <roobre@roobre.es>
2021-10-19 14:53:55 +0200

    linux/suse: error out for suse versions earlier than 15 Suse 12 repos are no longer up, and it's not possible to install dependencies (e.g. wget) from them

--- linux/helper_suse.sh
+++ linux/helper_suse.sh
@@ -1,7 +1,5 @@
 # Adds the NR repo
 add_repo() {
-    zypper -n install wget gnupg
-
     env=""
     if [ "$STAGING_REPO" = "true" ]; then
         env="-staging"
@@ -10,13 +8,27 @@ add_repo() {
 
     # Extract version and SP from docker image
     version=$(echo "$BASE_IMAGE" | grep -oE 'sles?[0-9]+' | grep -oE '[0-9]+')
+    if [ -z "$version" ]; then
+        printf "Unable to parse Suse version from BASE_IMAGE tag '%s'" "$BASE_IMAGE"
+        return 1
+    fi
+
+    # Versions earlier than 15 are deprecated and their repos are down. E.g. installing wget is not possible.
+    if [ "$version" -lt 15 ]; then
+        echo "Only Suse versions 15 and higher are supported by this action"
+        return 2
+    fi
+
     sp=$(echo "$BASE_IMAGE" | grep -oE 'sp[0-9]+' | grep -oE '[0-9]+')
     if [ -n "$sp" ]; then
         version="${version}.${sp}"
     fi
 
+    printf "Detected version '%s' from docker tag" "$version"
+
     sed -i "s/__VERSION__/$version/" /etc/yum.repos.d/newrelic-infra.repo
 
+    zypper -n install wget gnupg
     wget -nv -O- http://nr-downloads-main.s3-website-us-east-1.amazonaws.com/infrastructure_agent/gpg/newrelic-infra.gpg |  gpg --import
     zypper --gpg-auto-import-keys ref
     zypper -n ref -r newrelic-infra
