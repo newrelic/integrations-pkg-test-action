91c01b0742ab5274b11fde977294f4bfa9826eee
Juan Manuel Perez <jperezflorez@newrelic.com>
2021-10-21 22:37:17 +0200

    add support for suse 12 again

--- linux/helper_suse.sh
+++ linux/helper_suse.sh
@@ -1,37 +1,35 @@
 # Adds the NR repo
 add_repo() {
     env=""
+    domain="download.newrelic.com"
     if [ "$STAGING_REPO" = "true" ]; then
         env="-staging"
-    fi
-    cp "newrelic-infra-suse${env}.repo" /etc/zypp/repos.d/newrelic-infra.repo
-
-    # Extract version and SP from docker image
-    version=$(echo "$BASE_IMAGE" | grep -oE 'sles?[0-9]+' | grep -oE '[0-9]+')
-    if [ -z "$version" ]; then
-        printf "Unable to parse Suse version from BASE_IMAGE tag '%s'" "$BASE_IMAGE"
-        return 1
+        domain="nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com"
     fi
 
-    # Versions earlier than 15 are deprecated and their repos are down. E.g. installing wget is not possible.
-    if [ "$version" -lt 15 ]; then
-        echo "Only Suse versions 15 and higher are supported by this action"
-        return 2
+    # os-release file should have this structure:
+    #   NAME="SLES"
+    #   VERSION="15-SP3"
+    #   VERSION_ID="15.3"
+    #   PRETTY_NAME="SUSE Linux Enterprise Server 15 SP3"
+    #   ID="sles"
+    #   ID_LIKE="suse"
+    #   ANSI_COLOR="0;32"
+    #   CPE_NAME="cpe:/o:suse:sles:15:sp3"
+    #   DOCUMENTATION_URL="https://documentation.suse.com/"
+    . /etc/os-release
+    major_version="$(echo -n "$VERSION_ID" | cut -d. -f1)"
+    printf "Detected version '%s' from os-release" "$VERSION_ID"
+
+    if [ "$major_version" -eq 15 ]; then
+        zypper -n install wget
+        wget -qO- "http://${domain}/infrastructure_agent/gpg/newrelic-infra.gpg" | gpg --import
+        zypper -n addrepo "http://${domain}/infrastructure_agent/linux/zypp/sles/${VERSION_ID}/x86_64/newrelic-infra.repo"
+        zypper --gpg-auto-import-keys ref
+    elif [ "$major_version" -eq 12 ]; then
+        sed -e "s/__VERSION__/${VERSION_ID}/g" "newrelic-infra-suse-12${env}.repo" > /etc/zypp/repos.d/newrelic-infra.repo
+        zypper -n ref -r newrelic-infra
     fi
-
-    sp=$(echo "$BASE_IMAGE" | grep -oE 'sp[0-9]+' | grep -oE '[0-9]+')
-    if [ -n "$sp" ]; then
-        version="${version}.${sp}"
-    fi
-
-    printf "Detected version '%s' from docker tag" "$version"
-
-    sed -i "s/__VERSION__/$version/" /etc/yum.repos.d/newrelic-infra.repo
-
-    zypper -n install wget gnupg
-    wget -nv -O- http://nr-downloads-main.s3-website-us-east-1.amazonaws.com/infrastructure_agent/gpg/newrelic-infra.gpg |  gpg --import
-    zypper --gpg-auto-import-keys ref
-    zypper -n ref -r newrelic-infra
 }
 
 install_agent() {
