71a21f6329c04e78615fa3389cd16532d8e1185f
Juan Manuel Perez <jperezflorez@newrelic.com>
2021-10-22 12:52:31 +0200

    resolve review issues

--- linux/helper_suse.sh
+++ linux/helper_suse.sh
@@ -1,7 +1,7 @@
 # Adds the NR repo
 add_repo() {
     env=""
-    domain="download.newrelic.com"
+    domain="nr-downloads-main.s3-website-us-east-1.amazonaws.com"
     if [ "$STAGING_REPO" = "true" ]; then
         env="-staging"
         domain="nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com"
@@ -21,17 +21,14 @@ add_repo() {
     major_version="$(echo -n "$VERSION_ID" | cut -d. -f1)"
     printf "Detected version '%s' from os-release" "$VERSION_ID"
 
-    if [ "$major_version" -eq 15 ]; then
+    if [ "$major_version" -lt 15 ]; then
+        sed -e "s/__VERSION__/${VERSION_ID}/g" "newrelic-infra-suse-12${env}.repo" > /etc/zypp/repos.d/newrelic-infra.repo
+        zypper -n ref -r newrelic-infra
+    else
         zypper -n install wget
         wget -qO- "http://${domain}/infrastructure_agent/gpg/newrelic-infra.gpg" | gpg --import
         zypper -n addrepo "http://${domain}/infrastructure_agent/linux/zypp/sles/${VERSION_ID}/x86_64/newrelic-infra.repo"
         zypper --gpg-auto-import-keys ref
-    elif [ "$major_version" -eq 12 ]; then
-        sed -e "s/__VERSION__/${VERSION_ID}/g" "newrelic-infra-suse-12${env}.repo" > /etc/zypp/repos.d/newrelic-infra.repo
-        zypper -n ref -r newrelic-infra
-    else
-        echo "Only Suse versions 12 and 15 are supported by this action"
-        return 1
     fi
 }
 
