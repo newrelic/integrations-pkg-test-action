17543b8beeeb7ef47f4e318a0f7c110e72a5896c
sairaj18 <sairajsunny18@gmail.com>
2024-12-04 10:21:42 +0530

    removed destPrefix for staging and updated newrelic infra systemd version

--- linux/helper_ubuntu.sh
+++ linux/helper_ubuntu.sh
@@ -9,7 +9,7 @@ add_repo() {
 
     apt update && apt -y install wget gnupg
     if [ "$STAGING_REPO" = "true" ]; then
-        repo="http://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/${DEST_PREFIX}linux/apt"
+        repo="http://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/linux/apt"
     else
         repo="http://nr-downloads-main.s3-website-us-east-1.amazonaws.com/${DEST_PREFIX}linux/apt"
     fi
@@ -24,7 +24,7 @@ install_agent() {
     # TODO: Use the repo version when the staging repo gets fixed, since the systemd issue is workarounded
     # apt install -y newrelic-infra
 
-    AGENT_PACKAGE=${AGENT_PACKAGE:-newrelic-infra_systemd_1.15.2_systemd_amd64.deb}
+    AGENT_PACKAGE=${AGENT_PACKAGE:-newrelic-infra_systemd_1.58.0_systemd_amd64.deb}
     wget -nv "http://nr-downloads-main.s3-website-us-east-1.amazonaws.com/infrastructure_agent/linux/apt/pool/main/n/newrelic-infra/${AGENT_PACKAGE}"
     apt install "./${AGENT_PACKAGE}"
 }
