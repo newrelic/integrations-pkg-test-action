c0c2b9284544113eaf7fa785fee432b2616c747c
kmahale-nr <kmahale@newrelic.com>
2026-09-23 13:52:01 +0530

    fix: replace deprecated apt-key with SHA-256 signed keyring (#53)
    * linux(Debain 13): replace deprecated apt-key with SHA-256 signed-by keyring
    
    * linux: drop stale comment, add debian:trixie to default distros

--- linux/action.sh
+++ linux/action.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # Populate defaults
 [[ -n $GITHUB_ACTION_PATH ]] || GITHUB_ACTION_PATH=$(pwd)
 # TODO: https://new-relic.atlassian.net/browse/NR-616220
-[[ -n $DISTROS ]] || DISTROS="ubuntu:jammy ubuntu:focal ubuntu:bionic debian:bookworm rockylinux:8 registry.suse.com/suse/sles12sp5:latest suse"
+[[ -n $DISTROS ]] || DISTROS="ubuntu:jammy ubuntu:focal ubuntu:bionic debian:bookworm debian:trixie rockylinux:8 registry.suse.com/suse/sles12sp5:latest suse"
 [[ -n $PKGDIR ]] || PKGDIR="./dist"
 [[ -n $PACKAGE_LOCATION ]] || PACKAGE_LOCATION="local"
 
