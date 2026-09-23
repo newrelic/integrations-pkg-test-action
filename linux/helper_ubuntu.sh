cd6dd44e009fcf8c5231177162ef5d5034c82eee
kmahale <kmahale@newrelic.com>
2026-09-23 08:59:31 +0530

    linux: drop stale comment, add debian:trixie to default distros

--- linux/helper_ubuntu.sh
+++ linux/helper_ubuntu.sh
@@ -14,8 +14,6 @@ add_repo() {
         repo="http://nr-downloads-main.s3-website-us-east-1.amazonaws.com/infrastructure_agent/linux/apt"
     fi
 
-    # Production GPG key with SHA-256 signing:
-    #http://nr-downloads-main.s3-website-us-east-1.amazonaws.com/infrastructure_agent/gpg/newrelic-infra-sha256.gpg
     mkdir -p /etc/apt/keyrings
     wget -nv -O- http://nr-downloads-main.s3-website-us-east-1.amazonaws.com/infrastructure_agent/gpg/newrelic-infra-sha256.gpg | gpg --dearmor -o /etc/apt/keyrings/newrelic-infra.gpg
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/newrelic-infra.gpg] $repo $version main" > /etc/apt/sources.list.d/newrelic-infra.list
